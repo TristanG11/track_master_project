56d87511f7f5ff49751c9d832b8875c657af1e8d
TristanG11 <tristanogandagaguerick@gmail.com>
2025-10-05 14:11:28 +0200

    update

---
+++ src/extern/ydlidar_os30a/eYs3D_wrapper/build/CMakeFiles/pipeline.test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pipeline.test.dir/src/pipeline_main.cpp.o"
+  "CMakeFiles/pipeline.test.dir/src/pipeline_main.cpp.o.d"
+  "pipeline.test"
+  "pipeline.test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pipeline.test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
