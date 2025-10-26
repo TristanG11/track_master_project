cd092be8dfa415f3d9b8f63e73ccef4973ab9d8b
TristanG11 <tristanogandagaguerick@gmail.com>
2025-10-26 12:39:33 +0100

    add app initializer

--- src/track_master_debug/track_master_debug/app_initializer.py
+++ src/track_master_debug/track_master_debug/app_initializer.py
@@ -2,7 +2,7 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
-from track_master_debug.srv import GetCmdType, GetCmdTypeResponse
+from track_master_debug.srv import GetCmdType
 
 
 class AppInitializer(Node):
@@ -10,7 +10,7 @@ class AppInitializer(Node):
         super().__init__("app_initializer")
 
         # Variable locale pour stocker la dernière commande
-        self.current_cmd_type = "app_joystick"
+        self.current_cmd_type = ""
 
         # Subscriber à /cmd_type
         self.cmd_type_sub = self.create_subscription(
@@ -23,19 +23,19 @@ class AppInitializer(Node):
         # Service /get_cmd_type
         self.cmd_type_srv = self.create_service(
             GetCmdType,
-            "/get_cmd_type",
-            self.handle_get_cmd_type
+            "/app_initializer/get_cmd_type",
+            self.get_cmd_type_callback
         )
 
-        self.get_logger().info("✅ AppInitializer node started. Waiting for service calls...")
+        self.get_logger().info("AppInitializer node started. Waiting for service calls...")
 
     def cmd_type_callback(self, msg: String):
         """Met à jour la valeur courante de cmd_type"""
         self.current_cmd_type = msg.data
-        self.get_logger().info(f"📩 /cmd_type updated: {self.current_cmd_type}")
+        self.get_logger().info(f" /cmd_type updated: {self.current_cmd_type}")
 
-    def handle_get_cmd_type(self, request, response: GetCmdTypeResponse):
-        self.get_logger().info(f"🟢 Service /get_cmd_type called, returning: {self.current_cmd_type}")
+    def get_cmd_type_callback(self, request, response):
+        self.get_logger().info(f" Service /get_cmd_type called, returning: {self.current_cmd_type}")
         response.cmd_type = self.current_cmd_type
         return response
 
